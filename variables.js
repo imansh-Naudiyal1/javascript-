4c2deab10c4439ef92c60ba09300aee945183b80
Himanshu Naudiyal <harishnaudiyal2@gmail.com>
2025-03-17 11:49:03 +0530

    variables

---
+++ variables.js
@@ -0,0 +1,14 @@
+const accountId = 987654;
+let accountEmail = "rakesh@gmail.com";
+var accountPassword = "12345";
+
+
+//console.log(accountEmail);
+
+accountEmail = "abcd@gmail.com";
+accountPassword = "987654";
+accountCity = "jaipur";
+
+//console.log([accountEmail,accountId,accountPassword]);
+
+console.table([accountEmail,accountId,accountPassword]);
