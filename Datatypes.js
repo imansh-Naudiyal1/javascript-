87b0cf5f0e83fbf4834676c17511f9cd4fb9eb9a
Himanshu Naudiyal <harishnaudiyal2@gmail.com>
2025-03-17 14:27:43 +0530

    Data Types

---
+++ Datatypes.js
@@ -0,0 +1,25 @@
+//Numbers
+
+let age = 21;
+//console.log(age);
+//  know of what is the type of age (typeof)
+
+//string
+let Name = "Himanshu Naudiyal";
+//console.log( typeof Name);
+
+//Bigint
+let price = BigInt(99999999);
+//console.log(typeof price);
+
+//Boolean
+let isloogedIn = true;
+//console.log( typeof isloogedIn);
+
+//undefined
+let Name1;
+console.log(typeof Name1);
+
+//NUll
+let a = null;
+console.log(a);
