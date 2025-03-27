a35df62779ae5142108abf2ac763e03809b0f8b5
Himanshu Naudiyal <harishnaudiyal2@gmail.com>
2025-03-27 13:16:38 +0530

    arrays

---
+++ Array.js
@@ -0,0 +1,29 @@
+//Declaration
+ let number = [100,200,300,400,500];
+
+ // array index always start with 0 .....
+ let arr = [10,20,30,40,50];
+ //console.log(arr);
+
+ // we can access the value of element an array
+let a = ["apple","Vivo","Samsung"];
+//console.log(a [1] );
+
+//modifying the array
+let b = ["js","react","node.js"];
+//console.log(b);
+
+b[1] ="express"
+//console.log(b);
+
+//adding element using of .push() method
+
+let c = ["rahul","raju","shayam"];
+c.push("Ritu");                     
+c.unshift("riya")
+c.pop()
+console.log(c);
+
+// .push method adding ending  of the array
+// .unshift method adding to the starting of the array
+//.pop  removing the lasst element of array
