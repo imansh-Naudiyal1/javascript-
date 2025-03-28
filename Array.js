b49165c54a9195373445d57242d387dece87531b
Himanshu Naudiyal <harishnaudiyal2@gmail.com>
2025-03-28 12:25:04 +0530

    increase aad decrease the array of length

--- Array.js
+++ Array.js
@@ -22,8 +22,20 @@ let c = ["rahul","raju","shayam"];
 c.push("Ritu");                     
 c.unshift("riya")
 c.pop()
-console.log(c);
+//console.log(c);
 
 // .push method adding ending  of the array
 // .unshift method adding to the starting of the array
 //.pop  removing the lasst element of array
+
+//array length]
+
+let hi = ["a","b","c","d"];
+let length = hi.length;
+
+// console.log("array length: " + length);
+let lan = ["js","ruby","java","python"];
+//  lan.length = 7;
+//  console.log("after increasing the length: "  , lan);
+lan.length = 15;
+console.log("after decreasing the length: "  , lan);
